17f62e2aed621352de5b44569335374da95c7093
Benjamin Slawski <bslawski01@gmail.com>
2013-04-07 18:46:44 -0400

    basic tree navigation

--- app.py
+++ app.py
@@ -25,9 +25,60 @@ app.config['SECRET_KEY'] = '908u23r@32r2&//adfeofi21002'
 def main_page():
     conn = S3Connection('AKIAI6WMUYBUBJ2T3GMQ', 'hz0evOhGQTHgAeUeEZaJ0XIBv+wVU1C6iKyafe5b')    
     bucket = conn.get_bucket('photo-collective')
-    ## TODO: read bucket contents
+   
+    structure = {}
+    photo_list = {} 
+    keys = bucket.list()
+    for key in keys:
+        photo_path = key.name
+        splitpath = photo_path.split('/')[0:-1]
+        photo_path = []
 
-    return render_template('view_album.html')
+        sub_structure = structure
+        for dir0 in splitpath:
+            dir = str(dir0)
+            if not dir == 'image.jpg' and not dir == 'caption.txt':
+                photo_path.append(dir)
+                photo_path_str = str('/'.join(photo_path))
+                if not dir in photo_list:
+                    photo_list[dir] = photo_path_str
+
+                if not dir in sub_structure:
+                    sub_structure[dir] = {}
+                sub_structure = sub_structure[dir]
+
+    print structure, photo_list
+
+    session['data_tree'] = structure
+    return render_template('view_album.html', structure=structure, photo_list=photo_list)
+
+
+@app.route('/photo/', methods=['GET'])
+def get_photo():
+    photo_path = request.args.get('path')
+    splitpath = photo_path.split('/')
+
+    sub_structure = session['data_tree']
+    for dir in splitpath:
+        sub_structure = sub_structure[dir]
+
+    if 'image' in substructure:
+        return substructure['image']
+    return None
+
+
+@app.route('/caption/', methods=['GET'])
+def get_caption():
+    photo_path = request.args.get('path')
+    splitpath = photo_path.split('/')
+
+    sub_structure = session['data_tree']
+    for dir in splitpath:
+        sub_structure = sub_structure[dir]
+
+    if 'caption' in substructure:
+        return substructure['caption']
+    return None
 
 
 if __name__ == '__main__':
