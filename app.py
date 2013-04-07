188160429a40663c136fc99e45b99a1a33c66db2
Benjamin Slawski <bslawski01@gmail.com>
2013-04-07 01:03:12 -0400

    loads sample album on get

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from flask.ext.security.datastore.sqlalchemy import SQLAlchemyUserDatastore
 
 ## python modules
 import os
+from boto.s3.connection import S3Connection
 
 
 app = Flask(__name__)
@@ -19,15 +20,12 @@ app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = '908u23r@32r2&//adfeofi21002'
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
 def main_page():
-    if request.method == 'GET':
-        return render_template('upload_file.html')
-
-    elif request.method == 'POST':
-        file = request.files['file']
-        return 'One step at a time'
-
+    conn = S3Connection('AKIAI6WMUYBUBJ2T3GMQ', 'hz0evOhGQTHgAeUeEZaJ0XIBv+wVU1C6iKyafe5b')    
+    bucket = conn.get_bucket('photo-collective')
+    bucket.get_key('photos.zip').get_contents_as_string()
+    return render_template('view_album.html')
 
 
 if __name__ == '__main__':
